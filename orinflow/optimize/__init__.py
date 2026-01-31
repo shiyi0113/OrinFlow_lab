0a2c2fb661ded00f1c7368c0440c71cb8e5b0eca
shiyi0113 <shiyileshengxia@gmail.com>
2026-01-31 15:34:14 +0800

    wip...

--- orinflow/optimize/__init__.py
+++ orinflow/optimize/__init__.py
@@ -1,10 +1,7 @@
 """Model optimization utilities"""
 
 from .quantize import quantize_onnx
-from .sparsify import sparsify_model, export_sparse_model
 
 __all__ = [
     "quantize_onnx",
-    "sparsify_model",
-    "export_sparse_model",
 ]
